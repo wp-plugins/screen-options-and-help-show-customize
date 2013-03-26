6d34fca34e0c44add681311d2bdfd02b5605801b
gqevu6bsiz <gqevu6bsiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-26 01:45:39 +0000

    Link mistake.
    git-svn-id: https://plugins.svn.wordpress.org/screen-options-and-help-show-customize/tags/1.2.2@687314 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/settings.php
@@ -0,0 +1,164 @@
+<?php
+
+$PageTitle = __( 'Screen Options and Help Show Customize' , $this->ltd );
+
+// include js css
+$ReadedJs = array( 'jquery' , 'jquery-ui-sortable' );
+wp_enqueue_script( $this->Slug ,  $this->Dir . dirname( dirname( plugin_basename( __FILE__ ) ) ) . '.js', $ReadedJs , $this->Ver );
+wp_enqueue_style( $this->Slug , $this->Dir . dirname( dirname( plugin_basename( __FILE__ ) ) ) . '.css', array() , $this->Ver );
+
+// get data
+$Data = $this->get_data();
+
+?>
+
+<div class="wrap">
+	<div class="icon32" id="icon-options-general"></div>
+	<h2><?php echo $PageTitle; ?></h2>
+	<?php echo $this->Msg; ?>
+	<p><?php _e( 'Please set by clicking on the item you want to set.' , $this->ltd ); ?>
+	<p><?php _e( 'Please check the items you want to hide.' , $this->ltd ); ?>
+	<?php if( is_network_admin() ) : ?>
+		<p><strong><?php _e ( 'Data set in the network management screen is applied to all site management screen.' , $this->ltd ); ?></strong></p>
+	<?php endif; ?>
+
+	<form id="sohc" method="post" action="">
+		<input type="hidden" name="<?php echo $this->UPFN; ?>" value="Y">
+		<?php wp_nonce_field(); ?>
+
+		<div class="metabox-holder columns-2">
+
+			<div class="postbox-container" id="postbox-container-1">
+
+				<?php echo $this->get_lists( 'Dashboard' , 'dashboard' , $Data ); ?>
+				<?php if( defined( 'WP_ALLOW_MULTISITE' ) ) : ?>
+					<?php echo $this->get_lists( 'My Sites' , 'my-sites' , $Data ); ?>
+				<?php endif; ?>
+				<?php echo $this->get_lists( 'All Posts' , 'edit-post' , $Data ); ?>
+				<?php echo $this->get_lists( 'New Post' , 'post' , $Data ); ?>
+				<?php echo $this->get_lists( 'Categories' , 'edit-category' , $Data ); ?>
+				<?php echo $this->get_lists( 'Tags' , 'edit-post_tag' , $Data ); ?>
+				<?php echo $this->get_lists( 'Media Library' , 'upload' , $Data ); ?>
+				<?php echo $this->get_lists( 'Upload New Media' , 'media' , $Data ); ?>
+				<?php echo $this->get_lists( 'Links' , 'link-manager' , $Data ); ?>
+				<?php echo $this->get_lists( 'Add New Link' , 'link' , $Data ); ?>
+				<?php echo $this->get_lists( 'Link Categories' , 'edit-link_category' , $Data ); ?>
+				<?php echo $this->get_lists( 'Pages' , 'edit-page' , $Data ); ?>
+				<?php echo $this->get_lists( 'Add New Page' , 'page' , $Data ); ?>
+				<?php echo $this->get_lists( 'Comments' , 'edit-comments' , $Data ); ?>
+				<?php echo $this->get_lists( 'Edit Comment' , 'comment' , $Data ); ?>
+				<?php echo $this->get_lists( 'Manage Themes' , 'themes' , $Data ); ?>
+				<?php echo $this->get_lists( 'Install Themes' , 'theme-install' , $Data ); ?>
+				<?php echo $this->get_lists( 'Widgets' , 'widgets' , $Data ); ?>
+				<?php echo $this->get_lists( 'Menus' , 'nav-menus' , $Data ); ?>
+				<?php echo $this->get_lists( 'Users' , 'users' , $Data ); ?>
+				<?php echo $this->get_lists( 'Add New User' , 'user' , $Data ); ?>
+				<?php echo $this->get_lists( 'Profile' , 'profile' , $Data ); ?>
+				<?php echo $this->get_lists( 'Tools' , 'tools' , $Data ); ?>
+
+			</div>
+			
+			<div class="postbox-container" id="postbox-container-2">
+			
+				<div class="stuffbox" id="donationbox">
+					<div class="inside">
+						<p style="color: #FFFFFF; font-size: 20px;"><?php _e( 'Please donation.' , $this->ltd ); ?></p>
+						<p style="color: #FFFFFF;"><?php _e( 'You are contented with this plugin?<br />By the laws of Japan, Japan\'s new paypal user can not make a donation button.<br />So i would like you to buy this plugin as the replacement for the donation.' , $this->ltd ); ?></p>
+						<p>&nbsp;</p>
+						<p style="text-align: center;">
+							<a href="http://gqevu6bsiz.chicappa.jp/line-break-first-and-end/?utm_source=use_plugin&utm_medium=donate&utm_content=sohc&utm_campaign=1_2_2" class="button-primary" target="_blank">Line Break First and End</a>
+						</p>
+						<p>&nbsp;</p>
+						<div class="donation_memo">
+							<p><strong><?php _e( 'Features' , $this->ltd ); ?></strong></p>
+							<p><?php _e( 'Line Break First and End plugin is In the visual editor TinyMCE, It is a plugin that will help when you will not be able to enter a line break.' , $this->ltd ); ?></p>
+						</div>
+						<div class="donation_memo">
+							<p><strong><?php _e( 'The primary use of donations' , $this->ltd ); ?></strong></p>
+							<ul>
+								<li>- <?php _e( 'Liquidation of time and value' , $this->ltd ); ?></li>
+								<li>- <?php _e( 'Additional suggestions feature' , $this->ltd ); ?></li>
+								<li>- <?php _e( 'Maintain motivation' , $this->ltd ); ?></li>
+								<li>- <?php _e( 'Ensure time as the father of Sunday' , $this->ltd ); ?></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="stuffbox" id="aboutbox">
+					<h3><span class="hndle"><?php _e( 'About plugin' , $this->ltd ); ?></span></h3>
+					<div class="inside">
+						<p><?php _e( 'Version check' , $this->ltd ); ?> : 3.4.2 - 3.5.1</p>
+						<ul>
+							<li><a href="http://gqevu6bsiz.chicappa.jp/?utm_source=use_plugin&utm_medium=side&utm_content=sohc&utm_campaign=1_2_2" target="_blank"><?php _e( 'Developer\'s site' , $this->ltd ); ?></a></li>
+							<li><a href="http://wordpress.org/support/plugin/screen-options-and-help-show-customize" target="_blank"><?php _e( 'Support Forums' ); ?></a></li>
+							<li><a href="http://wordpress.org/support/view/plugin-reviews/screen-options-and-help-show-customize" target="_blank"><?php _e( 'Reviews' , $this->ltd ); ?></a></li>
+							<li><a href="https://twitter.com/gqevu6bsiz" target="_blank">twitter</a></li>
+							<li><a href="http://www.facebook.com/pages/Gqevu6bsiz/499584376749601" target="_blank">facebook</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="stuffbox" id="usefulbox">
+					<h3><span class="hndle"><?php _e( 'Useful plugins' , $this->ltd ); ?></span></h3>
+					<div class="inside">
+						<p><strong><a href="http://wordpress.org/extend/plugins/post-lists-view-custom/" target="_blank">Post Lists View Custom</a></strong></p>
+						<p class="description"><?php _e( 'Customize the list of the post and page. custom post type page, too. You can customize the column display items freely.' , $this->ltd ); ?></p>
+						<p><strong><a href="http://wordpress.org/extend/plugins/announce-from-the-dashboard/" target="_blank">Announce from the Dashboard</a></strong></p>
+						<p class="description"><?php _e( 'Announce to display the dashboard. Change the display to a different user role.' , $this->ltd ); ?></p>
+						<p><strong><a href="http://wordpress.org/extend/plugins/wp-admin-ui-customize/" target="_blank">WP Admin UI Customize</a></strong></p>
+						<p class="description"><?php _e( 'Customize a variety of screen management.' , $this->ltd ); ?></p>
+						<p>&nbsp;</p>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="clear"></div>
+
+		</div>
+
+		<p class="submit">
+			<input type="submit" class="button-primary" name="update" value="<?php _e( 'Save' ); ?>" />
+		</p>
+
+		<p class="submit reset">
+			<span class="description"><?php _e( 'Would initialize?' , $this->ltd ); ?></span>
+			<input type="submit" class="button-secondary" name="reset" value="<?php _e( 'Reset' ); ?>" />
+		</p>
+		
+	</form>
+
+	<p>&nbsp;</p>
+
+	<div class="postbox widget">
+		<div class="widget-top">
+			<div class="widget-title"><h4><?php _e( 'Import and Export' , $this->ltd ); ?></h4></div>
+		</div>
+		<div class="inside">
+
+			<p><strong><?php _e( 'Export' ); ?></strong></p>
+			<p>
+				<form id="file_export" method="get" action="">
+					<?php wp_nonce_field(); ?>
+					<input type="hidden" name="page" value="<?php echo $this->Slug; ?>" />
+					<input type="hidden" name="download" value="true" />
+					<input type="submit" class="button-secondary" name="export" value="<?php _e( 'Download Export File' ); ?>" />
+				</form>
+			</p>
+
+			<p><strong><?php _e( 'Import' ); ?></strong></p>
+			<p>
+				<form id="file_import" method="post" action="" enctype="multipart/form-data">
+					<?php wp_nonce_field(); ?>
+					<input type="hidden" name="upload" value="true" />
+					<label for="import"><?php _e( 'Choose a file from your computer:' ); ?></label>
+					<input type="file" name="import" size="25" />
+					<input type="submit" class="button-secondary" name="submit" value="<?php _e( 'Upload file and import' ); ?>" />
+				</form>
+			</p>
+
+		</div>
+	</div>
+
+</div>
---
+++ screen-options-and-help-show-customize.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+
+	var $Form = $("#sohc");
+
+	$('#postbox-container-1 .handlediv, #postbox-container-1 h3' , $Form).click(function() {
+		$(this).parent().toggleClass('closed');
+	});
+
+	$('input[name=all_checked]' , $Form).click(function() {
+
+		var $Checked = $(this).is(':checked');
+		var $Type = $(this).attr("class");
+		var $Screen_id = $(this).attr("title");
+		var $Table = $(this).parent().parent().parent().parent().parent();
+
+		$Table.children("tbody").children("tr").each(function() {
+			checkbox_id = $("input[type=checkbox][value=" + $Type + "][name*=" + $Screen_id + "]" , $(this) ).attr( "checked" , $Checked );
+		});
+
+	});
+
+});
